3348674c4f45f470b7bdd68ff360009b6bd10c99
IndyBoss <32159085+IndyBoss@users.noreply.github.com>
2018-03-08 05:48:00 +0100

    Homepage

--- Website/AntwerpenWeb/resources/views/home.blade.php
+++ Website/AntwerpenWeb/resources/views/home.blade.php
@@ -2,6 +2,12 @@
 
 @section('content')
 <div id="home">
+    <div id="homeText">
+        <div class="textFullWidth">
+            <h1>Seksuele intimidatie bij jongeren</h1>
+            <p>Dit is de site van Antwerpen over Seksuele intimidatie. Om meer te leren kan je onze verhalen en feiten lezen of The Game spelen hier onder.</p>
+        </div>
+    </div>
     <div id="buttons">
         <div class="buttonFullWidth">
             <button type="button" onclick="window.location.href='{{ route('info') }}'">Lees de verhalen</button>

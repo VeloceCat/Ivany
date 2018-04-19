c4cc3241a1fb8d9281db23e2f244fa8d04d4479a
IndyBoss <32159085+IndyBoss@users.noreply.github.com>
2018-04-19 09:53:54 +0200

    css

--- Website/AntwerpenWeb/resources/views/layouts/app.blade.php
+++ Website/AntwerpenWeb/resources/views/layouts/app.blade.php
@@ -13,6 +13,7 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/responsive.css') }}" rel="stylesheet">
 </head>
 <body>
     <div id="app">

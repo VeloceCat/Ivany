3450722501716dda37dd523726594103ea899b09
VeloceCat <velocecat@gmail.com>
2018-05-17 13:58:34 +0200

    contact page styling

--- Website/AntwerpenWeb/resources/views/contact.blade.php
+++ Website/AntwerpenWeb/resources/views/contact.blade.php
@@ -2,45 +2,45 @@
 
 @section('content')
 <div id="achtergrond">
-    <h1>Waar kan je terecht?</h1>
-    <h2>Heel wat mensen en organisaties staan voor je klaar.</h2>
-    <div class="infoBlok">
-        <div class="artikel">
+    <div class="infoBlok contactBlok">
+        <h1>Waar kan je terecht?</h1>
+        <h2 id="contactTitle2">Heel wat mensen en organisaties staan voor je klaar.</h2>
+        <div class="artikel contactLink">
             <h2>Jongerenadviescentrum (JAC)</h2>
             <p>Jongeren kunnen met al hun vragen terecht bij de Jongerenadviescentra (JAC) voor een <a href="http://www.jac.be/onthaal" target="_blank">gesprek, afspraak of chat</a>.</p>
             <p>Bezoek <a href="http://www.jac.be" target="_blank">JAC.be</a>.</p>
         </div>
-        <div class="artikel">
+        <div class="artikel contactLink">
             <h2>Awel</h2>
             <p>Awel luistert naar alle kinderen en jongeren met een vraag, een verhaal en/of een probleem. Ze werken volledig anoniem. Dit betekent dat enkel jij -als oproeper- en Awel ervan op de hoogte zijn dat jij met ons contact opneemt.</p>
             <p>Bezoek <a href="https://www.awel.be/" target="_blank">awel.be</a>.</p>
         </div>
-        <div class="artikel">
+        <div class="artikel contactLink">
             <h2>Pimento</h2>
             <p>Organisatie die zorgt voor informatie, participatie en vorming voor kinderen, jongeren en hun begeleiders. Ze hebben een ervaringsgericht vormingsaanbod.</p>
             <p>Bezoek <a href="https://www.pimento.be" target="_blank">pimento.be</a>.</p>
         </div>
-        <div class="artikel">
+        <div class="artikel contactLink">
             <h2>Sensoa</h2>
             <p>​Sensoa helpt om te praten over relaties, seksualiteit, grensoverschrijdend gedrag, seksuele diversiteit enzovoort. Ze bieden vormingsmateriaal aan en organiseren interessante opleidingen.</p>
             <p>Bezoek <a href="https://www.sensoa.be" target="_blank">sensoa.be</a> of <a href="https://www.seksuelevorming.be/" target="_blank">seksuele vorming.be</a>.</p>
         </div>
-        <div class="artikel">
+        <div class="artikel contactLink">
             <h2>Ella</h2>
-            <p>Expertise- en kenniscentrum voor gender en etniciteit. Het zet zich in voor bevordering van het emancipatie- en participatieproces van meisjes en vrouwen uit etnisch-culturele minderheidsgroepen in Vlaanderen. Ella biedt info en organiseert vormingen voor jongeren en intermediairs.</p>
+            <p>Expertise- en kenniscentrum voor gender en etniciteit. Het zet zich in voor bevordering van het emancipatie- en participatieproces van meisjes en vrouwen uit etnisch-culturele minderheidsgroepen in Vlaanderen.</p>
             <p>Bezoek <a href="http://www.ellavzw.be" target="_blank">ella vzw.be</a>.</p>
         </div>
-        <div class="artikel">
+        <div class="artikel contactLink">
             <h2>RoSa</h2>
             <p>RoSa vzw is een expertisecentrum voor gelijke kansen m/v, feminisme en gender. RoSa biedt ook workshops op maat aan voor iedereen die meer wil weten over deze thema’s.</p>
             <p>Bezoek <a href="https://www.rosavzw.be" target="_blank">RoSa vzw.be</a>.</p>
         </div>
-        <div class="artikel">
+        <div class="artikel contactLink">
             <h2>Cavaria</h2>
             <p>Koepelorganisatie van Vlaamse en Brusselse holebigroepen. Zij geeft vormingen in het onderwijs om het holebi- en transgenderthema bespreekbaar te maken in de klas en binnen de school.</p>
             <p>Bezoek <a href="https://www.cavaria.be/" target="_blank">cavaria.be</a>.</p>
         </div>
-        <div class="artikel">
+        <div class="artikel contactLink">
             <h2>Zelfmoordlijn</h2>
             <p>Dringend nood aan een gesprek? Bij de Zelfmoordlijn1813 staat er iemand voor je klaar. Elk gesprek is anoniem en gratis.</p>
             <p>Bezoek de <a href="https://www.zelfmoord1813.be" target="_blank">site</a>, <a href="mailto:indy.bosschem@gmail.com?Subject=Website%20visit" target="_top">email</a>, of bel naar 1813.</p>

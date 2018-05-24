350dfb020438843cf7d7c5f6d03c5416c6fe38bb
arthurvanpassel <32160634+arthurvanpassel@users.noreply.github.com>
2018-05-24 10:57:33 +0200

    favicon 2

--- Website/AntwerpenWeb/resources/views/layouts/app.blade.php
+++ Website/AntwerpenWeb/resources/views/layouts/app.blade.php
@@ -15,7 +15,7 @@
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
     <link href="{{ asset('css/responsive.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
-    <link rel="shortcut icon" href="{{ asset('/img/A_Logo_RGB.png') }}" >
+    <link rel="shortcut icon" href="{{ asset('/img/A_Logo.svg') }}" >
 </head>
 <body>
     <div id="app">

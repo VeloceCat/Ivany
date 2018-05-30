cd2b05004edef4b80f1e2dcefeb69131a50bcf23
VeloceCat <velocecat@gmail.com>
2018-05-30 12:24:51 +0200

    titles

--- Website/AntwerpenWeb/resources/views/posts/create.blade.php
+++ Website/AntwerpenWeb/resources/views/posts/create.blade.php
@@ -2,8 +2,6 @@
 
 @section('content')
 
-    <h2>Post toevoegen</h2>
-
     @include('posts._form', ['post' => $post])
 
 @stop
--- Website/AntwerpenWeb/resources/views/posts/edit.blade.php
+++ Website/AntwerpenWeb/resources/views/posts/edit.blade.php
@@ -2,8 +2,6 @@
 
 @section('content')
 
-    <h2>Post bewerken</h2>
     @include('posts._form', ['post' => $post])
-
 
 @stop

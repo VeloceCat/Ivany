d6613f70ad649f1bf4f358afadbb144a63b4223d
VeloceCat <velocecat@gmail.com>
2018-06-01 17:33:08 +0200

    styling login & register + responsive

--- Website/AntwerpenWeb/resources/views/auth/login.blade.php
+++ Website/AntwerpenWeb/resources/views/auth/login.blade.php
@@ -2,67 +2,67 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Login</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-sm-4 col-form-label text-md-right">E-Mail Adres</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
-
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">Wachtwoord</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="invalid-feedback">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    Wachtwoord vergeten?
-                                </a>
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Onthoud me
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Login
-                                </button>
-                            <p>Nog geen account? <a href="{{ route('register') }}">Registreer</a></p>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+    <div class="loginTable">
+        <div class="loginTable-left">
+            <div class="login-info">
+                <h2>Aanmelden met uw A-profiel</h2>
+                <p>Met een A-profiel krijgt u nieuws op maat, hoeft u niet steeds dezelfde gegevens in te vullen bij een aanvraag en kan u reageren op artikels en meepraten in de babbelbox.</p>
             </div>
+        </div>
+
+        <div class="loginTable-right">
+            <div class="login-fields">   
+                <form method="POST" action="{{ route('login') }}">
+                    @csrf
+
+                    <div class="form-group">
+                        <div>
+                            <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" placeholder="E-mail" required autofocus>
+
+                            @if ($errors->has('email'))
+                                <span class="invalid-feedback">
+                                    <strong>{{ $errors->first('email') }}</strong>
+                                </span>
+                            @endif
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <div>
+                            <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" placeholder="Wachtwoord" required>
+
+                            @if ($errors->has('password'))
+                                <span class="invalid-feedback">
+                                    <strong>{{ $errors->first('password') }}</strong>
+                                </span>
+                            @endif
+                            <a class="forgotPassword" href="{{ route('password.request') }}">
+                                <span>Wachtwoord vergeten?</span>
+                            </a>
+                        </div>
+                    </div>
+
+                    <div class="form-group mb-0">
+                        <div class="loginButton">
+                            <button type="submit" class="btn btn-primary">
+                                <span class="fa fa-user"></span>
+                                Aanmelden
+                            </button>
+                        </div>
+                    </div>
+                </form>
+                <p class="registerLink">Nog geen A-profiel? <a href="{{ route('register') }}"><span>Registreer uw A-profiel</span></a></p>
+                <div class="loginFooter">
+                    <p class="ng-scope">
+                        Met uw A-profiel kunt u aanmelden op deze websites:
+                    </p>
+                    <p>
+                        <a href="http://www.visitantwerpen.be" title="Visit Antwerpen">visitantwerpen.be span</a>
+                        <a href="https://www.gate15.be/nl/home" title="Gate15">gate15.be</a>
+                        <a href="https://www.slimnaarantwerpen.be/nl" title="Slim naar Antwerpen">slimnaarantwerpen.be</a>
+                    </p>
+                </div>
+            </div> 
         </div>
     </div>
 </div>
--- Website/AntwerpenWeb/resources/views/auth/register.blade.php
+++ Website/AntwerpenWeb/resources/views/auth/register.blade.php
@@ -2,103 +2,128 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Registreer</div>
+    <div class="registerTable">
+        <div class="registerTable-left">
+            <div class="register-info">
+                <h2>Registreren</h2>
+                <p>De gegevens in uw A-profiel, zoals naam en e-mailadres, worden nooit gedeeld met derden. De stad Antwerpen vindt privacy en de bescherming van uw gegevens erg belangrijk.</p>
+            </div>
+        </div> 
+        <div class="registerTable-right">
+            <div class="register-fields">
+                <form method="POST" action="{{ route('register') }}">
+                    @csrf
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
+                    <div class="form-group">
+                        <label for="name" class="col-form-label text-md-right">Voornaam <b> *</b></label>
 
-                        <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">Voornaam</label>
+                        <div>
+                            <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ old('name') }}" required autofocus>
 
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ old('name') }}" required autofocus>
+                            @if ($errors->has('name'))
+                                <span class="invalid-feedback">
+                                    <strong>{{ $errors->first('name') }}</strong>
+                                </span>
+                            @endif
+                        </div>
+                    </div>
 
-                                @if ($errors->has('name'))
-                                    <span class="invalid-feedback">
-                                        <strong>{{ $errors->first('name') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
+                    <div class="form-group">
+                        <label for="lastName" class="col-form-label text-md-right">Achternaam <b> *</b></label>
+
+                        <div>
+                            <input id="lastName" type="text" class="form-control{{ $errors->has('lastName') ? ' is-invalid' : '' }}" name="lastName" value="{{ old('lastName') }}" required autofocus>
+
+                            @if ($errors->has('lastName'))
+                                <span class="invalid-feedback">
+                                    <strong>{{ $errors->first('lastName') }}</strong>
+                                </span>
+                            @endif
+                        </div>
+                    </div>
+
+                    
+                    <div class="form-group">
+                        <label for="email" class="col-form-label text-md-right">E-Mailadres <b> *</b></label>
+
+                        <div>
+                            <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
+
+                            @if ($errors->has('email'))
+                                <span class="invalid-feedback">
+                                    <strong>{{ $errors->first('email') }}</strong>
+                                </span>
+                            @endif
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="username" class="col-form-label text-md-right">Anonieme schuilnaam <b> *</b></label>
+
+                        <div>
+                            <input id="username" type="text" class="form-control{{ $errors->has('username') ? ' is-invalid' : '' }}" name="username" value="{{ old('username') }}" required autofocus>
+
+                            @if ($errors->has('username'))
+                                <span class="invalid-feedback">
+                                    <strong>{{ $errors->first('username') }}</strong>
+                                </span>
+                            @endif
                         </div>
 
-                        <div class="form-group row">
-                            <label for="lastName" class="col-md-4 col-form-label text-md-right">Achternaam</label>
+                        <p class="uitlegSchuilnaam"><span>Uw gebruikersnaam moet tussen 5 en 50 karakters bevatten en kan bestaan uit letters, cijfers en volgende speciale tekens + . _ - @ (spaties zijn niet toegestaan).</span></p>
+                    </div>
 
-                            <div class="col-md-6">
-                                <input id="lastName" type="text" class="form-control{{ $errors->has('lastName') ? ' is-invalid' : '' }}" name="lastName" value="{{ old('lastName') }}" required autofocus>
 
-                                @if ($errors->has('lastName'))
-                                    <span class="invalid-feedback">
-                                        <strong>{{ $errors->first('lastName') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
+                    <div class="form-group">
+                        <label for="password" class="col-form-label text-md-right">Wachtwoord <b> *</b></label>
+
+                        <div>
+                            <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
+
+                            @if ($errors->has('password'))
+                                <span class="invalid-feedback">
+                                    <strong>{{ $errors->first('password') }}</strong>
+                                </span>
+                            @endif
                         </div>
+                        <p class="uitlegWachtwoord"><span>Het wachtwoord moet minstens 8 tekens lang zijn, 1 hoofdletter en 1 cijfer of symbool bevatten.</span></p>
+                    </div>
 
-                        <div class="form-group row">
-                            <label for="username" class="col-md-4 col-form-label text-md-right">Anonieme schuilnaam</label>
+                    <div class="form-group">
+                        <label for="password-confirm" class="col-form-label text-md-right">Bevestig wachtwoord <b> *</b></label>
 
-                            <div class="col-md-6">
-                                <input id="username" type="text" class="form-control{{ $errors->has('username') ? ' is-invalid' : '' }}" name="username" value="{{ old('username') }}" required autofocus>
-
-                                @if ($errors->has('username'))
-                                    <span class="invalid-feedback">
-                                        <strong>{{ $errors->first('username') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
+                        <div>
+                            <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
                         </div>
+                    </div>
 
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">E-Mail Adres</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
+                    <div class="form-group-full">
+                        <div>
+                            <input type="checkbox" class="field" name="terms" required>
+                            <span class="checkmark"></span>
+                            <label for="terms"><span>Ik ga akkoord met de <a href="https://aprofiel.antwerpen.be/nl/terms" target="_blank"><span>algemene voorwaarden</span></a><b> *</b></span></label>
                         </div>
+                    </div>
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">Wachtwoord</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="invalid-feedback">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">Bevestig wachtwoord</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Registreer
-                                </button>
-                                <p>Al een account? <a href="{{ route('login') }}">Log in</a></p>
-                            </div>
-                        </div>
-                    </form>
+                    <div class="form-group-full">
+                        <div>
+                        <div class="loginButton">
+                            <button type="submit" class="btn btn-primary">
+                                <span class="fa fa-user"></span>
+                                Registreren
+                            </button>
+                    </div>
+                </form>
+                <p class="registerLink">Al een A-profiel? <a href="{{ route('login') }}"><span>Login met uw A-profiel</span></a></p>
+                <div class="registerFooter">
+                    <p class="ng-scope">
+                        Met uw A-profiel kunt u aanmelden op deze websites:
+                    </p>
+                    <p>
+                        <a href="http://www.visitantwerpen.be" title="Visit Antwerpen">visitantwerpen.be span</a>
+                        <a href="https://www.gate15.be/nl/home" title="Gate15">gate15.be</a>
+                        <a href="https://www.slimnaarantwerpen.be/nl" title="Slim naar Antwerpen">slimnaarantwerpen.be</a>
+                    </p>
                 </div>
             </div>
         </div>

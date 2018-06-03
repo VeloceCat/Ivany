e7beef528b511a169132812375a67e6747142367
Indy Bosschem <indy.bosschem@student.kdg.be>
2018-06-03 19:56:06 +0200

    home fix

--- Website/AntwerpenWeb/resources/views/home.blade.php
+++ Website/AntwerpenWeb/resources/views/home.blade.php
@@ -6,7 +6,7 @@
         <div id="homeText">
             <div class="textFullWidth">
                 <h1>Seksuele intimidatie bij jongeren</h1>
-                <p>Bezoek onze info pagina voor allerlei verhalen. Speel het spel om je te verplaatsen in de shoenen van een slachtoffer.  We willen hierbij slachtoffers helpen, omstaanders leren begrijpen en mensen waarschuwen. Voor hulp kan je op onze contactpagina terecht.</p>
+                <p>Bezoek onze info pagina voor allerlei verhalen. Speel het spel om je te verplaatsen in de schoenen van een slachtoffer.  We willen hierbij slachtoffers helpen, omstaanders leren begrijpen en mensen waarschuwen. Voor hulp kan je op ons forum of op onze contactpagina terecht.</p>
             </div>
         </div>
         <div id="buttons">
